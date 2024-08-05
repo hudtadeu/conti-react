bd369f1bb0484773ea27bcd409e09ea3afec2dd4
Hudson Tadeu <hudsontadeu2010@gmail.com>
2024-08-05 14:03:49 -0300

    Atualização

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear, faChevronRight, faChevronDown, faRightFromBracket, faAddressBook, faTasks, faMagnifyingGlass, faChartBar, faChartPie, faRepeat, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
